04e19f0ed09a0c81d1beeafa92e07082277c104e
Mikhail Nersesov <mikhail.nersesov@gmail.com>
2023-11-12 19:42:47 +0100

    get Product file

---
+++ src/main/java/ru/yandex/praktikum/sprint3/Webinar/Product.java
@@ -0,0 +1,24 @@
+package ru.yandex.praktikum.sprint3.Webinar;
+
+public class Product {
+    private static final int MIN_PRICE = 100;
+    private final int price;
+    private final int discount;
+
+    public Product(int price, int discount) {
+        this.price = price;
+        this.discount = discount;
+    }
+    public int getFinalPrice(){
+        if(discount <= 0 || discount > 50){
+            return  price;
+        }
+        return price - price * discount / 100; // (0;50]
+    }
+    public int getPrice(){
+        if(price < MIN_PRICE){
+            return MIN_PRICE;
+        }
+        return price;
+    }
+}
